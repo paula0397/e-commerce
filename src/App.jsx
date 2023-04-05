fdf46da0a34c6dc1a5f2228b0ecde615c4297de0
Paula0397 <paula230397@gmail.com>
2023-04-05 11:29:47 -0500

    api

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { Box, Button, FormControl, Grid, IconButton, InputAdornment, InputLabel, OutlinedInput, Stack, TextField } from "@mui/material";
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import "./App.css";
+import { useState } from "react";
 function App() {
-  const [count, setCount] = useState(0)
+
+
+  const [showPassword, setShowPassword] = useState(false)
+
+  const handleClickShowPassword = () => setShowPassword((show) => !showPassword);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Box 
+        sx={{ 
+          width: "100%",
+          minHeight: "100vh",
+          padding:"10px 30px"
+          }}
+          >
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={6}>
+            <TextField label="Ingresa tu nombre" variant="outlined" fullWidth />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+          <TextField label="Ingresa tu email" variant="outlined" fullWidth />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+          <FormControl fullWidth variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-password"
+            type={showPassword ? "text" : "password"}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff color= "primary" /> : <Visibility color= "primary" />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Password"
+          />
+        </FormControl>
+          </Grid>
+        </Grid>
+
+        <Stack spacing={2} sx={{m:1}} direction="row">
+      <Button variant="contained">Enviar</Button>
+      <Button variant="outlined">Cancelar</Button>
+    </Stack>
+      </Box>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
